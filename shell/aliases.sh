d73101a07d3bfe90e6642a3ee62067666ff46710
Pierre-Luc Perrier <dev@the-pluc.net>
2018-01-10 17:08:54 +0100

    [shell] Add file `shell/aliases.sh`

---
+++ shell/aliases.sh
@@ -0,0 +1,12 @@
+# Common aliases for all shells
+
+alias df='df -hT'
+alias du='du -ch'
+alias https='http --default-scheme=https'
+alias la='ls -lah'
+alias ll='ls -lAh'
+alias ls='ls --color=auto --time-style=long-iso'
+alias mkdir='mkdir -v -p'
+alias mod='stat -L -c "%n %F %U(%u) %G(%g) %A(%a)"'
+alias mount='mount |column -t'
+alias netwatch='watch -d -t netstat -Wltunpe'
