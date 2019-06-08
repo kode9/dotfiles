f057dfdfe277ca0a9139af729bbb473f239e758a
Pierre-Luc Perrier <dev@the-pluc.net>
2019-06-08 14:02:18 +0200

    [web] Enable clipboard API
    Signed-off-by: Pierre-Luc Perrier <dev@the-pluc.net>

--- web/user.js
+++ web/user.js
@@ -110,7 +110,7 @@ user_pref("dom.event.clipboardevents.enabled",			false);
 // PREF: Disable "copy to clipboard" functionality via Javascript (Firefox >= 41)
 // NOTICE: Disabling clipboard operations will break legitimate JS-based "copy to clipboard" functionality
 // https://hg.mozilla.org/mozilla-central/rev/2f9f8ea4b9c3
-user_pref("dom.allow_cut_copy", false);
+user_pref("dom.allow_cut_copy", true);
 
 // PREF: Disable speech recognition
 // https://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
