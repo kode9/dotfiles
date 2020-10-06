98ee08d9666bdb2d0f928531534909b78fe739cb
Pierre-Luc Perrier <dev@the-pluc.net>
2020-10-06 22:02:51 +0200

    [shell] Rename `mount` alias to `mounted`

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -5,7 +5,7 @@ alias du='du -ch'
 alias https='http --default-scheme=https'
 alias mkdir='mkdir -v -p'
 alias mod='stat -L -c "%n %F %U(%u) %G(%g) %A(%a)"'
-alias mount='mount |column -t'
+alias mounted='mount |column -t'
 alias netwatch='watch -d -t netstat -Wltunpe'
 
 # ls / exa
