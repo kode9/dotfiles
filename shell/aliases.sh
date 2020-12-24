c46a9eca77582e42d9427c201a4ec53f78298eca
Pierre-Luc Perrier <dev@the-pluc.net>
2020-12-24 12:32:51 +0100

    [shell] Check that exa is installed before making aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -8,16 +8,32 @@ alias mod='stat -L -c "%n %F %U(%u) %G(%g) %A(%a)"'
 alias mounted='mount |column -t'
 alias netwatch='watch -d -t netstat -Wltunpe'
 
-# ls / exa
-alias exa='exa --color auto --color-scale --group-directories-first --level 2'
-alias ls='exa --grid --across'
-alias l='exa --long --group --time-style=iso --git'
-alias ll='l --grid --across'
-alias la='l --all'
-alias lla='l --all --grid --across'
-alias ltime='l --sort time --time-style=long-iso'
-alias lsize='l --sort size'
-alias ltype='l --sort extension'
-alias lr='l --recurse'
-alias ltree='l --tree'
-alias lfull='la --all --header -H -@'
+########################
+# exa (ls replacement) #
+########################
+if [ -x /usr/bin/exa ]; then
+  # Common options
+  alias exa='/usr/bin/exa --color auto --color-scale --group-directories-first --level 2'
+  # Column
+  alias l='exa --long --group --time-style=iso --git'
+  # Like default ls
+  alias lc='exa --grid --across'
+  # Grid
+  alias ll='l --grid --across'
+  # Column, hidden files
+  alias la='l --all'
+  # Grid, hidden files
+  alias lla='l --all --grid --across'
+  # Column, sorted by time
+  alias lti='l --sort time --time-style=long-iso'
+  # Column, sorted by size
+  alias lsi='l --sort size'
+  # Column, grouped by type
+  alias lty='l --sort extension'
+  # Column, recursive
+  alias lre='l --recurse'
+  # Tree
+  alias ltr='l --tree'
+  # Column, detailed informations
+  alias lfu='la --all --header -H -@'
+fi
